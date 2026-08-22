16dca1d3f1af4419b60eb1823479ffdb5a167939
alieldeenahmed <alieldeen.ah@gmail.com>
2026-08-22 13:39:20 +0300

    feat: add TasteProfile model

--- Backend/TikTokTasteProfiler/Models/TasteProfile.cs
+++ Backend/TikTokTasteProfiler/Models/TasteProfile.cs
@@ -3,4 +3,13 @@ namespace TikTokTasteProfiler.Models;
 public class TasteProfile
 {
     public int ID {get; set;}
+    public int AccountID {get; set;}
+    public int RepostsAnalyzed {get; set;}
+    public List<string> Likes {get; set;}
+    public List<string> Points {get; set;}
+    public List<string> Caveats {get; set;}
+    public List<string> Dislikes {get; set;}
+    public List<string> Preferences {get; set;}
+    public TikTokAccount Account {get; set;}
+    public DateTime GeneratedAtUTC {get; set;}
 }
